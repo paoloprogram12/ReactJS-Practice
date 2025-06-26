ac97e361cf0a98b3b85483561e99e6d2f77d1612
paoloprogram12 <sethpsalazar@gmail.com>
2025-06-26 11:08:24 -0700

    app.js

--- login-frontend/src/App.js
+++ login-frontend/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"; // react library
+import Signup from "./Signup"; // import the signup comonent
+import Login from "./Login"; // import the login component
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Login / Signup</h1> {/* Page Title */}
+      <Signup /> {/* signup form */}
+      <Login /> {/* login form */}
     </div>
   );
 }
