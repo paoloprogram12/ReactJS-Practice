0c836d4b70f2ec7abeb49e9f5931ec152b67ceb8
paoloprogram12 <sethpsalazar@gmail.com>
2025-07-01 19:09:27 -0700

    added a signout feature

--- login-frontend/src/Home.js
+++ login-frontend/src/Home.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
+    const navigate = useNavigate();
+
+    const handleSignOut = () => {
+        localStorage.removeItem("token"); // allows you to sign out
+
+        navigate("/login");
+    };
+
     return (
         <div>
             <h2>Sup</h2>
             <p>This is your home now...</p>
+
+        { /* Sign Out Button */ }
+        <button onClick={handleSignOut}>
+            Sign Out
+        </button>
         </div>
     )
 }
